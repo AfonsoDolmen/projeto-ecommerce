bf2464b25119feeb6f27e72db5c5e337a5e5b372
AfonsoDolmen <quadradoplay@outlook.com>
2024-12-17 14:38:45 -0300

    Desenvolvido backend de login.

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.views.generic import View
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
 
 
 class LoginView(View):
     def get(self, *args, **kwargs):
         return render(self.request, 'login.html')
     
-    def post(self, *args, **kwargs):
-        pass
+    def post(self, *args, **kwargs):    
+        # Coletando informações do formulário
+        email = self.request.POST.get('email')
+        password = self.request.POST.get('password')
+        
+        user_object = User.objects.get(email=email)
+        
+        # Verificando as credenciais
+        user = authenticate(self.request, username=user_object.username, password=password)
+
+        if user and email == user_object.email:
+            login(self.request, user)
+            return HttpResponseRedirect('/login/')
+        
+        return HttpResponseRedirect('/login/')

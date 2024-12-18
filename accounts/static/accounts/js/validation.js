63f04892e13c7da4ba0f80e2eca101a115b50ea4
Afonso Dolmen <quadradoplay@outlook.com>
2024-12-17 22:21:39 -0300

    Adicionado mais validações.

--- accounts/static/accounts/js/validation.js
+++ accounts/static/accounts/js/validation.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
     const emailField = this.getElementById('email-field');
     const passwordField = this.getElementById('password-field')
-    
+    const confirmPasswordField = this.getElementById('confirm-password-field');
+
     // Validação email
     emailField.addEventListener('input', function(){
         const emailValue = emailField.value
@@ -20,13 +21,27 @@ document.addEventListener('DOMContentLoaded', function(){
     passwordField.addEventListener('input', function(){
         const passwordLen = passwordField.value.length
 
-        if (passwordLen != 8) {
+        if (passwordLen < 8) {
             passwordField.classList.add('validation-error');
             passwordField.placeholder = "A senha deve conter no minimo 8 caracteres";
         } else if (passwordLen == 0) {
             passwordField.placeholder = "Digite sua senha";
         } else {
             passwordField.classList.add('validation-success');
+        }
+    });
+
+    // Validação confirmação senha
+    confirmPasswordField.addEventListener('input', function(){
+        const confirmPasswordValue = confirmPasswordField.value
+        const passwordValue = passwordField.value
+
+        if (passwordValue != confirmPasswordValue) {
+            confirmPasswordField.classList.add('validation-error');
+
+            confirmPasswordField.placeholder = "As senhas devem ser as mesmas!";
+        } else {
+            confirmPasswordField.classList.add('validation-success');
         }
     });
 });

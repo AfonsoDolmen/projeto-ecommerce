7ad34a574754464ce4e7cab80ab9b6ded4e21654
AfonsoDolmen <quadradoplay@outlook.com>
2024-12-18 15:11:31 -0300

    Rotas e View criadas.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from accounts.views import LoginView, RegisterView, LogoutView
 
 urlpatterns = [
-    path('', LoginView.as_view(), name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]

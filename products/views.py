f475fa53ede9076a2355a719583a0aaa6560ac09
AfonsoDolmen <quadradoplay@outlook.com>
2024-12-19 10:14:34 -0300

    Desenvolvido template e view de ListProduct.

--- products/views.py
+++ products/views.py
@@ -3,9 +3,20 @@ from products.models import Product
 
 class ProductsListView(ListView):
     model = Product
-    context_object_name = 'product'
+    context_object_name = 'products'
     template_name = 'products.html'
     paginate_by = 10
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        category = self.request.GET.get('category')
+
+        # Filtragem pela categoria
+        if category:
+            queryset = Product.objects.filter(category__category=category)
+
+        return queryset
 
 
 class ProductDetailView(DetailView):

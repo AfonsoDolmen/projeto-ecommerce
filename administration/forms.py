366ae2878e8c23ade36ca8ef8966d586908cff69
AfonsoDolmen <quadradoplay@outlook.com>
2025-01-12 18:27:49 -0300

    Desenvolvido template de admin_create.

--- administration/forms.py
+++ administration/forms.py
@@ -1,9 +1,8 @@
-from django.forms import ModelForm
-from django.core.exceptions import ValidationError
+from django import forms
 from products.models import Product
 
 
-class CreateProductForm(ModelForm):
+class CreateProductForm(forms.ModelForm):
     
     class Meta:
         model = Product
@@ -11,3 +10,33 @@ class CreateProductForm(ModelForm):
             'created_at',
             'updated_at',
             ]
+        
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite o título do produto',
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Descreva o produto',
+                'rows': 5,
+            }),
+            'image': forms.ClearableFileInput(attrs={
+                'class': 'form-control',
+            }),
+            'price': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite o preço do produto',
+            }),
+            'category': forms.Select(attrs={
+                'class': 'form-control',
+            }),
+            'stock': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite a quantidade em estoque',
+            }),
+            'slug': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite a url (ex: blusa-de-inverno)',
+            }),
+        }
